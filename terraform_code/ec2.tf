290254a9a16f496b58bea9a21af3f72b1e1a4632
anitha-sravanthi <chinthamallaanithasravanthi@gmail.com>
2024-10-18 19:10:59 +0530

    commit-3

---
+++ terraform_code/ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1" 
+    
+}
+
+resource "aws_instance" "demo-server"{
+    ami = "ami-005fc0f236362e99f"
+    instance_type ="t2.medium"
+    key_name = "dpp"
+}
